af12e2ab1f01a1cd13706b24ab1dd394c400f960
Abdul Hannan Ali <abdulhannanali@outlook.com>
2015-12-11 19:06:05 +0500

    remove config file

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = function () {
-  process.env.INCOMING_WEBHOOK =  "https://hooks.slack.com/services/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"
-  process.env.OUTGOING_TOKEN = "J5PL0kPpLFK3XMYEXcE1mNlF"
-}
