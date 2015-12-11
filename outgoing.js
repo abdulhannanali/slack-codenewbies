01732cc061fff8a8f87164e670f85f65bf1a5704
Abdul Hannan Ali <abdulhannanali@outlook.com>
2015-12-11 10:26:58 +0500

    add pledge registration functionality

--- outgoing.js
+++ outgoing.js
@@ -10,8 +10,23 @@ var HOST = process.env.HOST || "0.0.0.0";
 
 app.use(bodyParser());
 
+var users = {};
+
 app.use(function *(next) {
   console.log(this.request.body);
+  var user = this.request.body;
+  if (user.text == "!pledge") {
+    if (!users[user.user_name]) {
+      users[user.user_name] = true;
+      this.body = {"text": "You are now registered for this pledge"}
+    }
+    else {
+      this.body = {"text":"You are already registered for this pledge"}
+    }
+  }
+  else {
+    this.body = {"text": "let's get to the real talk. Type !pledge to take this pledge"}
+  }
 })
 
 server.listen(PORT, HOST, function (error) {
